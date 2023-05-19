e0c2af946a8f168bf83847ff84c93c7742e8f5a5
Sophi0 <s21hohlsofj@venta.lv>
2023-05-19 19:54:30 +0300

    New: created new project

---
+++ src/main/java/lv/venta/JavaHomework2Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHomework2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHomework2Application.class, args);
+	}
+
+}
