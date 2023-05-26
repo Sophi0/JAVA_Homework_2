08952d0d41a573377aa3275db6b4d5c10a00f5f9
Sophi0 <s21hohlsofj@venta.lv>
2023-05-26 22:09:34 +0300

    New: added addNew driver function post ang get

--- src/main/java/lv/venta/controller/DriverController.java
+++ src/main/java/lv/venta/controller/DriverController.java
@@ -3,9 +3,13 @@ package lv.venta.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
+import lv.venta.models.Driver;
 import lv.venta.services.IDriverCRUDService;
 
 @Controller
@@ -47,6 +51,26 @@ public class DriverController {
 		}
 	}
 	
+	@GetMapping(value = "/driver/addNew")	//localhost:8080/driver/addNew
+	public String getAddNewDriverFunc(Model model) {
+		model.addAttribute("drivers", new Driver());
+		return "add-driver-page";	//will call add-driver-page.html
+	}
 	
+	@PostMapping(value = "/driver/addNew")
+	public String postAddNewDriverFunc(@Valid Driver driver, BindingResult result) {
+		if(!result.hasErrors()) {
+			try {
+				driverCRUDService.addnewDriver(driver.getName(), driver.getSurname(), driver.getBcategory());
+				return "redirect:/driver/showAll";
+			}
+			catch (Exception e) {
+				return "redirect:/error-page";
+			}
+		}
+		else {
+			return "add-driver-page";
+		}
+	}
 	
 }
